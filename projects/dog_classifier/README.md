7292cf66bcc51ce9c1a67487e7c8ad62ff4a4b59
Hikmatyor Azatov <azatovhikmatyor@gmail.com>
2026-08-01 01:24:24 +0500

    cleaning

--- projects/dog_classifier/README.md
+++
@@ -1,1 +0,0 @@
-http://vision.stanford.edu/aditya86/ImageNetDogs/
--- projects/iris/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-from web.router import router
-
-app = FastAPI()
-
-app.mount('/static', StaticFiles(directory='web/static'), name='static')
-app.include_router(router)
